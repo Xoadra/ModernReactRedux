a023c5a70753755e861fbfa2fc1072ab87169b8d
Xoadra <xijp77@gmail.com>
2018-05-24 12:20:32 -0700

    Basic api search added to App component.

--- ReduxSimpleStarter/src/app.js
+++ ReduxSimpleStarter/src/app.js
@@ -4,10 +4,24 @@
 
 import React, { Component } from 'react'
 import Search from './components/search'
+import youtube from 'youtube-api-search'
 
+
+
+const key = 'AIzaSyD4XgV-Xd0ZGu5w7Ds7o4FeDfvgU8w8lV0'
 
 
 class App extends Component {
+	
+	constructor( props ) {
+		super( props )
+		this.state = { videos: [ ] }
+		youtube( { key: key, term: 'lizards' }, videos => {
+			// ES6 shortcut to assign keys and values with same variable name
+			this.setState( { videos } )
+		} )
+	}
+	
 	
 	render( ) {
 		return (
--- ReduxSimpleStarter/src/index.js
+++ ReduxSimpleStarter/src/index.js
@@ -8,10 +8,6 @@ import App from './app'
 
 
 
-const key = 'AIzaSyB8S5J1_MpGUdUJrFo1rSDEWlcil3072xA'
-
-
 ReactDOM.render( <App/>, document.getElementById( 'root' ) )
-
 
 
