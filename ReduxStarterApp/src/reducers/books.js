a45f82929f95a83b6466ee4341c3220267219a86
Xoadra <xijp77@gmail.com>
2018-05-24 18:04:24 -0700

    Wrote first basic Redux reducer.

---
+++ ReduxStarterApp/src/reducers/books.js
@@ -0,0 +1,17 @@
+
+
+
+
+export default function( ) {
+	return [
+		{ title: 'The First' },
+		{ title: 'The Second' },
+		{ title: 'The Third' },
+		{ title: 'The Fourth' },
+		{ title: 'The Fifth' },
+		{ title: 'The Sixth' },
+		{ title: 'The Seventh' }
+	]
+}
+
+
---
+++ ReduxStarterApp/src/reducers/index.js
@@ -0,0 +1,17 @@
+
+
+
+
+import { combineReducers } from 'redux'
+import booksReducer from './books'
+
+
+
+const rootReducer = combineReducers( {
+	books: booksReducer
+} )
+
+
+export default rootReducer
+
+
