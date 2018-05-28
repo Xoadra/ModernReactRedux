250afe47ec8e831054b7ae5863d325e597d8223b
Xoadra <xijp77@gmail.com>
2018-05-28 03:10:39 -0700

    New project set up for learning React Router.

---
+++ ReactRouterApp/src/app.js
@@ -0,0 +1,25 @@
+
+
+
+
+import React, { Component } from 'react'
+
+import '../public/style.css'
+
+
+
+class App extends Component {
+	
+	render( ) {
+		return (
+			<div> React Router App </div>
+		)
+	}
+	
+}
+
+
+export default App
+
+
+
