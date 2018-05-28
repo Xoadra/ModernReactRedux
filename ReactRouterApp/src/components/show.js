463d7ef9cd4f885270c418e6ef759bed8eefb404
Xoadra <xijp77@gmail.com>
2018-05-28 16:20:22 -0700

    Show component successfully renders post contents.

--- ReactRouterApp/src/components/show.js
+++ ReactRouterApp/src/components/show.js
@@ -3,21 +3,41 @@
 
 
 import React, { Component } from 'react'
+import { connect } from 'react-redux'
+
+import { grabPost } from '../actions/api'
 
 
 
 class Show extends Component {
 	
+	componentDidMount( ) {
+		const { id } = this.props.match.params
+		this.props.grabPost( id )
+	}
+	
 	render( ) {
+		const { post } = this.props
+		if ( !post ) {
+			return <div> Loading... </div>
+		}
 		return (
-			<div> Show Post </div>
+			<div>
+				<h3> { post.title } </h3>
+				<h6> Categories: { post.categories } </h6>
+				<p> { post.content } </p>
+			</div>
 		)
 	}
 	
 }
 
 
-export default Show
+function mapStateToProps( { posts }, ownProps ) {
+	return { post: posts[ ownProps.match.params.id ] }
+}
 
+
+export default connect( mapStateToProps, { grabPost } )( Show )
 
 
