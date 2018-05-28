940db3dd550712d0a0261c837099b281f2824a74
Xoadra <xijp77@gmail.com>
2018-05-28 08:12:57 -0700

    Validation conditions added to New component's form.

--- ReactRouterApp/src/components/new.js
+++ ReactRouterApp/src/components/new.js
@@ -15,6 +15,7 @@ class New extends Component {
 			<div className="form-group">
 				<label>{ field.label }</label>
 				<input className="form-control" type="text" { ...field.input }/>
+				{ field.meta.error }
 			</div>
 		)
 	}
@@ -23,7 +24,7 @@ class New extends Component {
 		return (
 			<form>
 				<Field label="Title" name="title" component={ this.renderField }/>
-				<Field label="Tags" name="tags" component={ this.renderField }/>
+				<Field label="Categories" name="categories" component={ this.renderField }/>
 				<Field label="Content" name="content" component={ this.renderField }/>
 			</form>
 		)
@@ -32,8 +33,27 @@ class New extends Component {
 }
 
 
+// Data submitted from the form is passed into validate as an object
+function validate( values ) {
+	const errors = {  }
+	// Validate any form inputs passed in through the values argument
+	if ( !values.title ) {
+		errors.title = 'Enter a title!'
+	}
+	if ( !values.categories ) {
+		errors.categories = 'Enter some categories!'
+	}
+	if ( !values.content ) {
+		errors.content = 'Enter post content!'
+	}
+	// If errors object is empty, it is assumed to be fine for submission
+	// Any properties in errors mean that the form submitted is invalid
+	return errors
+}
+
+
 // Form property should be unique to differentiate it from other forms
-export default reduxForm( { form: 'PostsNewForm' } )( New )
+export default reduxForm( { form: 'PostsNewForm', validate } )( New )
 
 
 
