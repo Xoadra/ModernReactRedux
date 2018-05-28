7b7099c48f41ac2dc870bb9d84aadc968895c1d4
Xoadra <xijp77@gmail.com>
2018-05-27 19:17:59 -0700

    Returned array data as state from weather reducer.

--- ReactReduxApp/src/reducers/weather.js
+++ ReactReduxApp/src/reducers/weather.js
@@ -2,8 +2,22 @@
 
 
 
-export default function( state = null, action ) {
-	console.log( 'Reducer fired with action result:', action )
+import { weatherFetch } from '../actions/fetch'
+
+
+
+export default function( state = [ ], action ) {
+	switch ( action.type ) {
+		case weatherFetch:
+			console.log( 'Reducer fired with action result:', action )
+			// Don't try to mutate state by doing this
+			/* return state.push( action.payload.data ) */
+			// This is supposedly a better alternative
+			/* return state.concat( [ action.payload.data ] ) */
+			// Another option using newer ES6 syntax
+			/* return [ action.payload.data, ...state ] */
+			return [ action.payload.data ].concat( state )
+	}
 	return state
 }
 
