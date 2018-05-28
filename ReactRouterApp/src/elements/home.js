df050ac42284b4c680b7eb69beeddf03c282f5aa
Xoadra <xijp77@gmail.com>
2018-05-28 06:02:02 -0700

    Home component ready for rendering all blog posts.

--- ReactRouterApp/src/elements/home.js
+++ ReactRouterApp/src/elements/home.js
@@ -4,6 +4,7 @@
 
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
+import _ from 'lodash'
 
 import { fetchPosts } from '../actions/fetch'
 
@@ -11,17 +12,42 @@ import { fetchPosts } from '../actions/fetch'
 
 class Home extends Component {
 	
+	// Executes immediately after the Home component finishes rendering
+	componentDidMount( ) {
+		this.props.fetchPosts( )
+	}
+	
+	renderPosts( ) {
+		return _.map( this.props.posts, post => {
+			return (
+				<li key={ post.id } className="list-group-item">
+					{ post.title }
+				</li>
+			)
+		} )
+	}
+	
 	render( ) {
+		console.log( this.props.post )
 		return (
-			<div> Home </div>
+			<div>
+				<h3> Posts </h3>
+				<ul className="list-group">
+					{ this.renderPosts( ) }
+				</ul>
+			</div>
 		)
 	}
 	
 }
 
 
-// Shortcut via ES6 when returning is all that mapDispatchToProps does
-export default connect( null, { fetchPosts } )( Home )
+function mapStateToProps( state ) {
+	return { posts: state.posts }
+}
 
+
+// Shortcut using ES6 when returning is all that mapDispatchToProps does
+export default connect( mapStateToProps, { fetchPosts } )( Home )
 
 
